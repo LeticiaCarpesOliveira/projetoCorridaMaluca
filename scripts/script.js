2aeb63fd283ad39802366b0fbc5a41d8648008ca
LeticiaCarpesOliveira <leticia.oliveira888@etec.sp.gov.br>
2025-06-02 13:48:26 -0300

    Adicionando Arquivos

---
+++ scripts/script.js
@@ -0,0 +1,16 @@
+const nav = document.querySelector(".navHeader");
+const menu = document.getElementById("menu");
+
+nav.classList.remove("open");
+
+
+menu.addEventListener("click", () => {
+    nav.classList.toggle("open");
+});
+
+
+window.addEventListener("scroll", () => {
+    if(nav.classList.contains("open")){
+        nav.classList.remove("open");
+    }
+});
